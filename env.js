0506a4a7058f64f6f9ac2df8cdb5250c08e20155
CaniCanRemaster <canicanremasterizado@gmail.com>
2024-03-29 10:46:49 -0600

    update | env

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 //_FILESYSTEM
 
-var SOUNDS_FILESYSTEM_HOST_PRODUCTION = "https://canicanremaster.github.io/music-sounds";
+var SOUNDS_FILESYSTEM_HOST_PRODUCTION = "https://josereza.github.io/music-sounds";
 var SOUNDS_FILESYSTEM_HOST_DEVELOPMENT = "http://localhost:4000";
 var SOUNDS_FILESYSTEM_HOST = (window.location.hostname == "localhost" || window.location.hostname == "127.0.0.1") ? SOUNDS_FILESYSTEM_HOST_DEVELOPMENT : SOUNDS_FILESYSTEM_HOST_PRODUCTION; 
 
@@ -10,18 +10,18 @@ var SCRIPTS_FILESYSTEM_HOST = (window.location.hostname == "localhost" || window
 
 //_FRONTEND
 
-var SOUNDS_FRONTEND_HOST_PRODUCTION = "https://canicanremaster.github.io/music-sounds";
+var SOUNDS_FRONTEND_HOST_PRODUCTION = "https://josereza.github.io/music-sounds";
 var SOUNDS_FRONTEND_HOST_DEVELOPMENT = "http://localhost:3500";
 var SOUNDS_FRONTEND_HOST = (window.location.hostname == "localhost" || window.location.hostname == "127.0.0.1") ? SOUNDS_FRONTEND_HOST_DEVELOPMENT : SOUNDS_FRONTEND_HOST_PRODUCTION; 
 
-var SCRIPTS_FRONTEND_HOST_PRODUCTION = "https://canicanremaster.github.io/music-scripts";
+var SCRIPTS_FRONTEND_HOST_PRODUCTION = "https://josereza.github.io/music-scripts";
 var SCRIPTS_FRONTEND_HOST_DEVELOPMENT = "http://localhost:3250";
 var SCRIPTS_FRONTEND_HOST = (window.location.hostname == "localhost" || window.location.hostname == "127.0.0.1") ? SCRIPTS_FRONTEND_HOST_DEVELOPMENT : SCRIPTS_FRONTEND_HOST_PRODUCTION; 
 
 //_PUBLISH
 
 var PUBLISH_HOST_DEVELOPMENT = "http://localhost:3000/src/assets/publish.html";
-var PUBLISH_HOST_PRODUCTION = "https://canicanremaster.github.io/music-software/src/assets/publish.html";
+var PUBLISH_HOST_PRODUCTION = "https://josereza.github.io/music-software/src/assets/publish.html";
 var PUBLISH_HOST = (window.location.hostname == "localhost" || window.location.hostname == "127.0.0.1") ? PUBLISH_HOST_DEVELOPMENT : PUBLISH_HOST_PRODUCTION; 
 
 //_OAUTH
